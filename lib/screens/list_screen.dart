2a37a6580389e335ac6024dcd34b81fe16215e3f
PippasSong <tlmag2108@gmail.com>
2022-08-28 20:32:45 +0900

    sql 패키지, 메인 화면 작성 중

--- lib/screens/list_screen.dart
+++ lib/screens/list_screen.dart
@@ -1,10 +1,34 @@
 import "package:flutter/material.dart";
+import "package:flutter_mini_project_1/models/todo.dart";
 
-class ListScreen extends StatelessWidget {
+class ListScreen extends StatefulWidget {
+  @override
+  _ListScreenState createState() => _ListScreenState();
+}
+
+class _ListScreenState extends State<ListScreen> {
+  List<Todo> todos = [];
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("ListScreen")),
+    return Scaffold(
+      appBar: AppBar(title: Text('할 일 목록 앱'), actions: [
+        InkWell(
+          onTap: () {},
+          child: Container(
+              padding: EdgeInsets.all(5),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [Icon(Icons.book), Text('뉴스')],
+              )),
+        )
+      ]),
     );
   }
 }
